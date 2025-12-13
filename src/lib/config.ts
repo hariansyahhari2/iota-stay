fa1ed5fa53c4cb56becfabf648c824e41bd55f02
hariansyah <hariansyahhari2@gmail.com>
2025-12-14 00:48:08 +0700

    add real testnet iota package id

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,7 +10,7 @@ import { createNetworkConfig } from "@iota/dapp-kit"
 // Package IDs
 export const DEVNET_PACKAGE_ID = ""
 // export const TESTNET_PACKAGE_ID = "0x6d5afd0fcb56bce5426a2ccf59dcc4972a61b7bb724993483186c621fa2d452c"
-export const TESTNET_PACKAGE_ID = "0x1040f03b1cbc55932ec340487aa770a48b9b392e43ca11a86e7f8e672cbf539d"
+export const TESTNET_PACKAGE_ID = "0x7b223702cca87d2bc7e4047035d84fed158c4edc8807745aafac4b70701fff80"
 export const MAINNET_PACKAGE_ID = ""
 
 // Network configuration
