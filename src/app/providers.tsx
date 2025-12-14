b4957e0539a1748da056a1c37038684f05238aaa
Hariansyah Hariansyah <hariansyah15001@gmail.com>
2025-12-14 06:35:05 +0000

    I think the modifier is networks not networkConfig

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,13 +3,18 @@
 import { IotaProvider } from '@/lib/iota';
 import { IotaClientProvider, WalletProvider } from '@iota/dapp-kit';
 import { networkConfig } from '@/lib/config';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <IotaClientProvider networkConfig={networkConfig} defaultNetwork="testnet">
-      <WalletProvider>
-        <IotaProvider>{children}</IotaProvider>
-      </WalletProvider>
-    </IotaClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <IotaClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>
+          <IotaProvider>{children}</IotaProvider>
+        </WalletProvider>
+      </IotaClientProvider>
+    </QueryClientProvider>
   );
 }
