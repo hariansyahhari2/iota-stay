2c41d99d029f21666a8403822869e530b4f1eace
Hariansyah Hariansyah <hariansyah15001@gmail.com>
2025-12-14 09:38:41 +0000

    changee testnet package id 0x0bd789e4392dd4e8829b6cb317356e695a4954e57bd

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,7 +10,7 @@ import { createNetworkConfig } from "@iota/dapp-kit"
 // Package IDs
 export const DEVNET_PACKAGE_ID = ""
 // export const TESTNET_PACKAGE_ID = "0x6d5afd0fcb56bce5426a2ccf59dcc4972a61b7bb724993483186c621fa2d452c"
-export const TESTNET_PACKAGE_ID = "0x7b223702cca87d2bc7e4047035d84fed158c4edc8807745aafac4b70701fff80"
+export const TESTNET_PACKAGE_ID = "0x0bd789e4392dd4e8829b6cb317356e695a4954e57bdc78c834e6d5b28722a695"
 export const MAINNET_PACKAGE_ID = ""
 
 // Network configuration
